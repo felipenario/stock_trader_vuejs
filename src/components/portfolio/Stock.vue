0f4278a73e125f2127f2fd489c6affca8c9a6b94
felipenario <felipe.nario@novaxs.com.br>
2020-10-26 17:50:53 -0300

    Initial commit

---
+++ src/components/portfolio/Stock.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+    <v-card class="blue darken-3 white--text">
+      <v-card-title class="headline">
+        <strong
+          >{{ stock.name }}
+          <small
+            >(Preço: {{ stock.price | currency }} | Qtde:
+            {{ stock.quantity }})</small
+          ></strong
+        >
+      </v-card-title>
+      <v-card>
+        <v-container fill-height>
+          <v-text-field
+            label="Quantidade"
+            type="number"
+            v-model.number="quantity"
+            :error="
+              quantity < 0 ||
+              !Number.isInteger(quantity) ||
+              insufficientQuantity
+            "
+          ></v-text-field>
+          <v-btn
+            class="blue darken-3 white--text"
+            @click="sellStock"
+            :disabled="
+              quantity <= 0 ||
+              !Number.isInteger(quantity) ||
+              insufficientQuantity
+            "
+            >{{ insufficientQuantity ? "Insuficiente" : "Vender" }}</v-btn
+          >
+        </v-container>
+      </v-card>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+//import {mapActions} from 'vuex'
+
+export default {
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0,
+    };
+  },
+  computed: {
+    insufficientQuantity() {
+      return this.quantity > this.stock.quantity;
+    },
+  },
+  methods: {
+    //mapActions({ sellStockAction: 'sellStock' }) mapeando action mudando seu nome para não ter conflito.
+    sellStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity,
+      };
+      //this.sellStockAction(order)
+      this.$store.dispatch("sellStock", order);
+      this.quantity = 0;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
